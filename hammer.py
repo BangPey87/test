05e9cd3dbc99dbd22529801f12f2291eca51a57a
BangPey87 <50002893+BangPey87@users.noreply.github.com>
2019-05-11 07:12:19 +0700

    Update hammer.py

--- hammer.py
+++ hammer.py
@@ -77,8 +77,8 @@ def usage():
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
 	usage : python3 hammer.py [-s] [-p] [-t]
-	-h : help
-	-s : server ip
+	-h : tolong
+	-s : ip target cuy
 	-p : port default 80
 	-t : turbo default 135 \033[0m''')
 	sys.exit()
